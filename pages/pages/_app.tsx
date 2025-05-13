a5ccaa210021a020f032afc03fea8547fa1e064b
The-CARPARK <the.carpark2025@gmail.com>
2025-05-13 22:19:48 +1200

    md update

---
+++ pages/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+import { MDXProvider } from '@mdx-js/react'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MDXProvider>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
+}
